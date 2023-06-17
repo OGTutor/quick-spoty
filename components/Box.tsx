61753c2d8e6e729d8b383446d77dc346dcbe3827
OGTutor <i.send.you.a.letter@gmail.com>
2023-06-17 21:18:24 +0300

    Implemented a Box component

---
+++ components/Box.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+interface BoxProps {
+	children: React.ReactNode;
+	className?: string;
+}
+
+const Box: FC<BoxProps> = ({ className, children }) => {
+	return (
+		<div
+			className={twMerge(
+				`bg-neutral-900 rounded-lg
+			h-fit w-full`,
+				className
+			)}
+		>
+			{children}
+		</div>
+	);
+};
+
+export default Box;
