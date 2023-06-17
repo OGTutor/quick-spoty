0d8a4b722edc6bb39d8ee8e9848f492c0604525b
OGTutor <i.send.you.a.letter@gmail.com>
2023-06-17 21:14:14 +0300

    Added a Sidebar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import { Figtree } from 'next/font/google';
 
+import Sidebar from '@/components/Sidebar';
+
 import './globals.css';
 
 const font = Figtree({ subsets: ['latin'] });
@@ -16,7 +18,9 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className={font.className}>{children}</body>
+			<body className={font.className}>
+				<Sidebar>{children}</Sidebar>
+			</body>
 		</html>
 	);
 }
