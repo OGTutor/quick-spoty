dfaadc141c516bfffda093b60711f5ffe258e377
OGTutor <i.send.you.a.letter@gmail.com>
2023-06-23 20:55:40 +0300

    Implemented a UserProvider provider

---
+++ providers/UserProvider.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { FC } from 'react';
+
+import { MyUserContextProvider } from '@/hooks/useUser';
+
+interface UserProviderProps {
+	children: React.ReactNode;
+}
+
+const UserProvider: FC<UserProviderProps> = ({ children }) => {
+	return <MyUserContextProvider>{children}</MyUserContextProvider>;
+};
+
+export default UserProvider;
