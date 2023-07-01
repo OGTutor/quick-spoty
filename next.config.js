15fc8da7b7e933bdabf4e6ec5d82d16a7c51ca2a
OGTutor <i.send.you.a.letter@gmail.com>
2023-07-01 23:27:02 +0300

    Added a new image domain

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		domains: ['luqiuqizuiahakdeftgh.supabase.co'],
+	},
+};
 
 module.exports = nextConfig;
