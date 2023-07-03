3620e117560b21f034ee36ca7f3ddd8fcc14eefb
OGTutor <i.send.you.a.letter@gmail.com>
2023-07-03 22:36:10 +0300

    Implemented a SearchContent component

---
+++ app/search/components/SearchContent.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { FC } from 'react';
+import { Song } from 'types';
+
+import MediaItem from '@/components/MediaItem';
+
+interface SearchContentProps {
+	songs: Song[];
+}
+
+const SearchContent: FC<SearchContentProps> = ({ songs }) => {
+	if (songs.length === 0) {
+		return (
+			<div className="flex flex-col gap-y-2">
+				<h1 className="px-6 text-white text-3xl font-semibold">
+					No Results
+				</h1>
+				<div className="w-full px-6 text-neutral-400">
+					Try a new search.
+				</div>
+			</div>
+		);
+	}
+
+	return (
+		<div
+			className="flex flex-col gap-y-2
+			w-full px-6"
+		>
+			{songs.map((song) => (
+				<div
+					key={song.id}
+					className="flex items-center 
+					gap-x-4 w-full"
+				>
+					<div className="flex-1">
+						<MediaItem onClick={() => {}} data={song} />
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default SearchContent;
