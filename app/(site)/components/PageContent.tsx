7a90b298ade5208569410908f5719d857ecb3d6f
OGTutor <i.send.you.a.letter@gmail.com>
2023-07-10 23:39:52 +0300

    Implemented onPlay functionality

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -5,11 +5,15 @@ import { Song } from 'types';
 
 import SongItem from '@/components/SongItem';
 
+import useOnPlay from '@/hooks/useOnPlay';
+
 interface PageContentProps {
 	songs: Song[];
 }
 
 const PageContent: FC<PageContentProps> = ({ songs }) => {
+	const onPlay = useOnPlay(songs);
+
 	if (songs.length === 0) {
 		return <div className="mt-4 text-neutral-400">No songs available.</div>;
 	}
@@ -21,7 +25,11 @@ const PageContent: FC<PageContentProps> = ({ songs }) => {
 			2xl:grid-cols-8 gap-4 mt-4"
 		>
 			{songs.map((song) => (
-				<SongItem key={song.id} onClick={() => {}} data={song} />
+				<SongItem
+					key={song.id}
+					onClick={(id: string) => onPlay(id)}
+					data={song}
+				/>
 			))}
 		</div>
 	);
