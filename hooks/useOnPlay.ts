bbb20891cc5d0084dbed5cd64680a6c4d88a7a18
OGTutor <i.send.you.a.letter@gmail.com>
2023-07-18 22:40:50 +0300

    Added subscribeModal entity

--- hooks/useOnPlay.ts
+++ hooks/useOnPlay.ts
@@ -2,15 +2,19 @@ import { Song } from 'types';
 
 import useAuthModal from './useAuthModal';
 import usePlayer from './usePlayer';
+import useSubscribeModal from './useSubscribeModal';
 import { useUser } from './useUser';
 
 const useOnPlay = (songs: Song[]) => {
 	const player = usePlayer();
+	const subscribeModal = useSubscribeModal();
 	const authModal = useAuthModal();
-	const { user } = useUser();
+	const { user, subscription } = useUser();
 
 	const onPlay = (id: string) => {
 		if (!user) return authModal.onOpen();
+
+		if (!subscription) return subscribeModal.onOpen();
 
 		player.setId(id);
 		player.setIds(songs.map((song) => song.id));
